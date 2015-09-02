a401c0dfdc53beae9eb647dd0ec79c32a469f648
Sam Strauss <strausssa@s.dcsdk12.org>
2015-09-02 11:00:06 -0600

    I am a boss

--- methods_quiz.rb
+++ methods_quiz.rb
@@ -1,15 +1,73 @@
 module MethodsQuiz
-	
-	# TODO - write has_teen?
-	
-	# TODO - write not_string
-	
-	# TODO - write icy_hot?
-	
-	# TODO - write closer_to
 
-	# TODO - write two_as_one?
+#1
+	def has_teen(d, e, f)
+		if (d >= 13 && d <= 19) || (e >= 13 && e <= 19) || (f >= 13 && f <= 19)
+			return true
+		else
+			return false
+		end
 
-	# TODO - write pig_latinify
+	end
 
+
+
+#2
+	def not_string(strings)
+		if strings.start_with?("not")
+			return strings
+		else
+			return "not" + strings
+		end
+	end
+
+
+
+
+#3
+	def icy_hot?(a, b)
+		if (a >= 100 && b <= 0) || (b >= 100 && a <= 0)
+			return true
+		else
+			return false
+		end
+	end
+	
+#4
+	def closer_to(answer, g5, g6)
+		close1 = (g5 - answer).abs
+		close2 = (g6 - answer).abs
+		if close1 < close2
+			return g5
+		elsif close2 < close1
+			return g6
+		else
+			return 0
+		end
+	end
+
+#5
+	def two_as_one?(a, b, c)
+		ab = a + b
+		bc = b + c
+		ac = a + c
+	if ab == c || bc == a || ac == b
+		return true
+	else
+		return false
+	end
+	end
+
+	def pig_latinify(newstring)
+		str = newstring.downcase.gsub(/\s+/, "")
+		if str.start_with?("a") || str.start_with?("e") || str.start_with?("i") || str.start_with?("o") || str.start_with?("u")
+			return str + "way"
+		elsif str == "shit" || str == "fuck" || str == "ass" || str == "hell" || str == "damn" || str == "dick" || str == "pussy"
+			return str.gsub("a", "*").gsub("e", "*").gsub("i", "*").gsub("o", "*").gsub("u", "*")
+		else
+			suffix = str[0]
+			str[0] = ""
+			return str + suffix + "ay"
+		end
+	end
 end
--- methods_quiz_test.rb
+++ methods_quiz_test.rb
@@ -12,5 +12,52 @@ class MethodsQuizTest < MiniTest::Test
 	end
 
 	# TODO - write tests here
+	#1
+	def test_has_teen
+		assert_equal false, @m.has_teen(1, 2, 3)
+		assert_equal true, @m.has_teen(12, 45, 17)
+		assert_equal false, @m.has_teen(10, 20, 12)
+		assert_equal true, @m.has_teen(13, 16, 19)
+	end
 
+#2
+	def test_not_string
+		assert_equal "notasdf", @m.not_string("notasdf")
+		assert_equal "not hey", @m.not_string("hey")
+		assert_equal "not yo", @m.not_string("not yo")
+		assert_equal "not this is cool, not", @m.not_string("this is cool, not")
+	end
+
+#3
+	def test_icy_hot?
+		assert_equal false, @m.icy_hot?(-25, 78)
+		assert_equal true, @m.icy_hot?(0, 100)
+		assert_equal false, @m.icy_hot?(6, 5876745)
+		assert_equal true, @m.icy_hot?(-32, 134)
+	end
+
+#4
+	def test_closer_to
+		assert_equal 34, @m.closer_to(45, 34, 67)
+		assert_equal 0, @m.closer_to(30, 78, 78)
+		assert_equal 123, @m.closer_to(30, 79697, 123)
+		assert_equal 0, @m.closer_to(6, -8, -8)
+	end
+
+#5
+	def test_two_as_one?
+		assert_equal true, @m.two_as_one?(4, 5, 9)
+		assert_equal false, @m.two_as_one?(7, 8, 3)
+		assert_equal true, @m.two_as_one?(8, 3, 5)
+		assert_equal true, @m.two_as_one?(-9, -10, -1)
+	end
+
+	def test_pig_latinify
+		assert_equal "eyhay", @m.pig_latinify(" hey ")
+		assert_equal "oyay", @m.pig_latinify("yo")
+		assert_equal "orangeway", @m.pig_latinify("orange")
+		assert_equal "iphoneway", @m.pig_latinify(" iPhone")
+
+		assert_equal "sh*t", @m.pig_latinify("shit")
+	end
 end
